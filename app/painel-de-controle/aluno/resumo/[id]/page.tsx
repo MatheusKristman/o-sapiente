5fd47de8182450602bd6f47612a97feec13babb8
João Falcão <jgabfalcao@gmail.com>
2023-10-04 12:46:54 -0300

    feat: Student dashboard boxes

--- app/painel-de-controle/aluno/resumo/[id]/page.tsx
+++ app/painel-de-controle/aluno/resumo/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { Dot } from "lucide-react";
@@ -11,26 +11,120 @@ const DashboardPage = () => {
       <div className="w-full flex flex-col-reverse gap-9">
         <div className="w-full flex flex-col gap-5 bg-white rounded-2xl p-9 shadow-md shadow-[rgba(0,0,0,0.25)]">
           <div className="relative aspect-square overflow-hidden rounded-2xl">
-            <Image src="/assets/images/profile-test.png" alt="Perfil" fill className="object-cover" />
+            <Image
+              src="/assets/images/profile-test.png"
+              alt="Perfil"
+              fill
+              className="object-cover"
+            />
           </div>
 
-          <span className="w-full text-center text-xl text-gray-primary font-semibold">John Doe</span>
+          <span className="w-full text-center text-xl text-gray-primary font-semibold">
+            John Doe
+          </span>
         </div>
 
         <div className="shadow-md shadow-[rgba(0,0,0,0.25)] rounded-lg">
-          <Button label="NOVA SOLICITAÇÃO" primary fullWidth onClick={() => {}} />
+          <Button
+            label="NOVA SOLICITAÇÃO"
+            primary
+            fullWidth
+            onClick={() => {}}
+          />
         </div>
       </div>
 
-      <div className="w-full flex flex-col gap-9">
-        <div className="w-full rounded-lg bg-green-primary p-9">
-          <h2 className="text-white text-xl font-semibold mb-5 md:text-2xl">Solicitações Pendentes</h2>
+      <div className="w-full flex flex-col gap-8">
+        <div className="w-full rounded-lg bg-green-primary p-9 mb-5 shadow-md shadow-[rgba(0,0,0,0.25)]">
+          <h2 className="text-white text-2xl font-semibold mb-5 md:text-3xl">
+            Solicitações <br />
+            Pendentes
+          </h2>
 
-          <div className="w-full h-72 scrollbar scrollbar-thumb-slate-100">
+          <div className="w-full h-56 scrollbar scrollbar-thumb-slate-100">
+            <div className="w-full rounded-lg bg-white p-5">
+              <div className="flex flex-col">
+                <div className="flex justify-center">
+                  <Image
+                    src="/assets/images/profile-test.png"
+                    alt="Perfil"
+                    width={50}
+                    height={40}
+                    className="object-cover rounded-3xl"
+                  />
+                </div>
+
+                <div className="flex items-center justify-center p-2.5 text-green-primary text-lg font-semibold">
+                  <span>John Doe</span>
+
+                  <Dot />
+
+                  <span>Matemática</span>
+                </div>
+              </div>
+              <div className="flex items-center justify-center">
+                <Button
+                  primary
+                  fullWidth
+                  label="VER PROPOSTA"
+                  onClick={() => {}}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full rounded-lg bg-white p-9 mb-9 shadow-md shadow-[rgba(0,0,0,0.25)]">
+          <h2 className=" text-2xl text-green-primary font-semibold mb-5 md:text-3xl">
+            Aulas em <br />
+            Andamento
+          </h2>
+
+          <div className="w-full h-56 scrollbar scrollbar-thumb-slate-100">
+            <div className="w-full rounded-lg bg-green-primary p-5">
+              <div className="flex flex-col">
+                <div className="flex justify-center">
+                  <Image
+                    src="/assets/images/profile-test.png"
+                    alt="Perfil"
+                    width={50}
+                    height={40}
+                    className="object-cover rounded-3xl"
+                  />
+                </div>
+
+                <div className="flex items-center justify-center p-2.5 text-white text-lg font-semibold">
+                  <span>John Doe</span>
+
+                  <Dot />
+
+                  <span>Matemática</span>
+                </div>
+              </div>
+              <div className="flex items-center justify-center">
+                <Button
+                  primaryMobile
+                  fullWidth
+                  label="VER PROPOSTA"
+                  onClick={() => {}}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* TODO continuar estilização */}
+        {/* 
             <div>
               <div>
                 <div>
-                  <Image src="/assets/images/profile-test.png" alt="Perfil" width={20} height={20} className="object-cover" />
+                  <Image
+                    src="/assets/images/profile-test.png"
+                    alt="Perfil"
+                    width={20}
+                    height={20}
+                    className="object-cover"
+                    />
                 </div>
 
                 <div>
@@ -45,29 +139,16 @@ const DashboardPage = () => {
               <Button label="VER MENSAGEM" onClick={() => {}} />
             </div>
 
-            {/* TODO continuar estilização */}
             <div>
               <div>
                 <div>
-                  <Image src="/assets/images/profile-test.png" alt="Perfil" width={20} height={20} className="object-cover" />
-                </div>
-
-                <div>
-                  <span>John Doe</span>
-
-                  <Dot />
-
-                  <span>Matemática</span>
-                </div>
-              </div>
-
-              <Button label="VER MENSAGEM" onClick={() => {}} />
-            </div>
-
-            <div>
-              <div>
-                <div>
-                  <Image src="/assets/images/profile-test.png" alt="Perfil" width={20} height={20} className="object-cover" />
+                  <Image
+                    src="/assets/images/profile-test.png"
+                    alt="Perfil"
+                    width={20}
+                    height={20}
+                    className="object-cover"
+                  />
                 </div>
 
                 <div>
@@ -82,7 +163,7 @@ const DashboardPage = () => {
               <Button label="VER MENSAGEM" onClick={() => {}} />
             </div>
           </div>
-        </div>
+        
 
         <div>
           <h2>Aulas em Andamento</h2>
@@ -91,7 +172,13 @@ const DashboardPage = () => {
             <div>
               <div>
                 <div>
-                  <Image src="/assets/images/profile-test.png" alt="Perfil" width={20} height={20} className="object-cover" />
+                  <Image
+                    src="/assets/images/profile-test.png"
+                    alt="Perfil"
+                    width={20}
+                    height={20}
+                    className="object-cover"
+                  />
                 </div>
 
                 <div>
@@ -106,10 +193,10 @@ const DashboardPage = () => {
               <Button label="VER MENSAGEM" onClick={() => {}} />
             </div>
           </div>
-        </div>
+            */}
       </div>
     </div>
-  )
+  );
 };
 
 export default DashboardPage;
