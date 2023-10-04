88a6e91dd9d95e5917ae8ed6f34c4a569b2f9189
João Falcão <jgabfalcao@gmail.com>
2023-10-04 17:27:40 -0300

    feat: Desktop Student dashboard boxes

--- app/painel-de-controle/aluno/resumo/[id]/page.tsx
+++ app/painel-de-controle/aluno/resumo/[id]/page.tsx
@@ -7,8 +7,8 @@ import Button from "@/components/Button";
 
 const DashboardPage = () => {
   return (
-    <div className="flex-1 w-full px-6 pt-9 mx-auto flex flex-col gap-9 md:px-16 lg:container">
-      <div className="w-full flex flex-col-reverse gap-9">
+    <div className="flex-1 w-full px-6 pt-9 mx-auto flex flex-col gap-9 md:flex-row md:px-16 lg:container">
+      <div className="w-full flex flex-col-reverse gap-9 md:flex-col xl:w-6/12">
         <div className="w-full flex flex-col gap-5 bg-white rounded-2xl p-9 shadow-md shadow-[rgba(0,0,0,0.25)]">
           <div className="relative aspect-square overflow-hidden rounded-2xl">
             <Image
@@ -36,14 +36,13 @@ const DashboardPage = () => {
 
       <div className="w-full flex flex-col gap-8">
         <div className="w-full rounded-lg bg-green-primary p-9 mb-5 shadow-md shadow-[rgba(0,0,0,0.25)]">
-          <h2 className="text-white text-2xl font-semibold mb-5 md:text-3xl">
-            Solicitações <br />
-            Pendentes
+          <h2 className="text-white text-2xl font-semibold mb-5 md:text-3xl lg:whitespace-nowrap whitespace-normal">
+            Solicitações Pendentes
           </h2>
 
           <div className="w-full h-56 scrollbar scrollbar-thumb-slate-100">
             <div className="w-full rounded-lg bg-white p-5">
-              <div className="flex flex-col">
+              <div className="flex flex-col lg:flex-row lg:gap-6">
                 <div className="flex justify-center">
                   <Image
                     src="/assets/images/profile-test.png"
@@ -54,35 +53,34 @@ const DashboardPage = () => {
                   />
                 </div>
 
-                <div className="flex items-center justify-center p-2.5 text-green-primary text-lg font-semibold">
+                <div className="flex items-center justify-center p-2.5 text-green-primary text-lg font-semibold lg:p-1">
                   <span>John Doe</span>
 
-                  <Dot />
+                  <Dot style={{ width: "35px", height: "35px" }} />
 
-                  <span>Matemática</span>
+                  <span className="text-base">Matemática</span>
                 </div>
-              </div>
-              <div className="flex items-center justify-center">
-                <Button
-                  primary
-                  fullWidth
-                  label="VER PROPOSTA"
-                  onClick={() => {}}
-                />
+                <div className="flex items-center justify-center">
+                  <Button
+                    primary
+                    fullWidth
+                    label="VER PROPOSTA"
+                    onClick={() => {}}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
 
         <div className="w-full rounded-lg bg-white p-9 mb-9 shadow-md shadow-[rgba(0,0,0,0.25)]">
-          <h2 className=" text-2xl text-green-primary font-semibold mb-5 md:text-3xl">
-            Aulas em <br />
-            Andamento
+          <h2 className=" text-2xl text-green-primary font-semibold mb-5 md:text-3xl lg:whitespace-nowrap whitespace-normal">
+            Aulas em Andamento
           </h2>
 
           <div className="w-full h-56 scrollbar scrollbar-thumb-slate-100">
             <div className="w-full rounded-lg bg-green-primary p-5">
-              <div className="flex flex-col">
+              <div className="flex flex-col lg:flex-row lg:gap-6">
                 <div className="flex justify-center">
                   <Image
                     src="/assets/images/profile-test.png"
@@ -93,21 +91,21 @@ const DashboardPage = () => {
                   />
                 </div>
 
-                <div className="flex items-center justify-center p-2.5 text-white text-lg font-semibold">
+                <div className="flex items-center justify-center p-2.5 text-white text-lg font-semibold lg:p-1">
                   <span>John Doe</span>
 
-                  <Dot />
+                  <Dot style={{ width: "35px", height: "35px" }} />
 
-                  <span>Matemática</span>
+                  <span className="text-base">Matemática</span>
                 </div>
-              </div>
-              <div className="flex items-center justify-center">
-                <Button
-                  primaryMobile
-                  fullWidth
-                  label="VER PROPOSTA"
-                  onClick={() => {}}
-                />
+                <div className="flex items-center justify-center">
+                  <Button
+                    primaryMobile
+                    fullWidth
+                    label="VER MENSAGENS"
+                    onClick={() => {}}
+                  />
+                </div>
               </div>
             </div>
           </div>
