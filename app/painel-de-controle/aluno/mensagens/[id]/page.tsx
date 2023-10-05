bbcf42d538c509d7afe8137d81db834c026607b6
João Falcão <jgabfalcao@gmail.com>
2023-10-05 17:00:27 -0300

    feat: Messages section mobile

--- app/painel-de-controle/aluno/mensagens/[id]/page.tsx
+++ app/painel-de-controle/aluno/mensagens/[id]/page.tsx
@@ -1,117 +1,135 @@
 "use client";
 
 import Image from "next/image";
-import { Dot } from "lucide-react";
+import { Dot, Search } from "lucide-react";
 
 import Button from "@/components/Button";
 
 const DashboardPage = () => {
   return (
-    <div className="flex-1 w-full px-6 pt-9 mx-auto flex flex-col gap-9 md:flex-row md:px-16 lg:container">
-      <div className="w-full flex flex-col-reverse gap-9 md:flex-col lg:w-4/12 xl:w-6/12">
-        <div className="w-full flex flex-col gap-5 bg-white rounded-2xl p-9 shadow-md shadow-[rgba(0,0,0,0.25)]">
-          <div className="relative aspect-square overflow-hidden rounded-2xl">
-            <Image
-              src="/assets/images/profile-test.png"
-              alt="Perfil"
-              fill
-              className="object-cover"
+    <div className="flex-1 w-full pt-9 mx-auto flex flex-col gap-9 md:flex-row md:px-16 lg:container">
+      <div className="w-full px-6">
+        <div className="pt-2 relative mx-auto ">
+          <input
+            type="search"
+            name="search"
+            placeholder="Pesquise o aluno aqui..."
+            className="w-full border-2 border-gray-200 bg-gray-200 h-10 px-5 pr-16 rounded-lg text-md focus:outline-none"
+          />
+          <button type="submit" className="absolute right-0 top-0 mt-4 mr-4">
+            <Search
+              style={{
+                color: "#9DA5AA",
+              }}
             />
-          </div>
-
-          <span className="w-full text-center text-xl text-gray-primary font-semibold">
-            John Doe
-          </span>
-        </div>
-
-        <div className="shadow-md shadow-[rgba(0,0,0,0.25)] rounded-lg">
-          teste
+          </button>
         </div>
       </div>
+      <div className="w-full h-56 scrollbar scrollbar-thumb-slate-100">
+        <div className="w-full bg-white hover:bg-green-primary transition ease-in-out delay-150 py-6 group">
+          <div className="flex flex-row w-full">
+            <div className="flex justify-start px-6 w-3/12">
+              <Image
+                src="/assets/images/profile-test.png"
+                alt="Perfil"
+                width={50}
+                height={50}
+                className="object-cover rounded-3xl lg:w-12 lg:h-12"
+              />
+            </div>
 
-      <div className="w-full flex flex-col gap-8">
-        <div className="w-full rounded-lg bg-green-primary p-9 mb-5 shadow-md shadow-[rgba(0,0,0,0.25)]">
-          <h2 className="text-white text-2xl font-semibold mb-5 md:text-3xl lg:whitespace-nowrap whitespace-normal">
-            Solicitações Pendentes
-          </h2>
+            <div className="flex flex-col w-5/12">
+              <span className="font-semibold">John Doe</span>
+              <span className="text-sm whitespace-nowrap">
+                Gostei das aulas, muito...
+              </span>
+            </div>
 
-          <div className="w-full h-56 scrollbar scrollbar-thumb-slate-100">
-            <div className="w-full rounded-lg bg-white p-5">
-              <div className="flex flex-col lg:flex-row lg:gap-6 xl:w-full">
-                <div className="flex justify-center xl:w-1/12">
-                  <Image
-                    src="/assets/images/profile-test.png"
-                    alt="Perfil"
-                    width={50}
-                    height={40}
-                    className="object-cover rounded-3xl lg:w-12 lg:h-12"
-                  />
-                </div>
-
-                <div className="flex items-center justify-center p-2.5 text-green-primary text-lg font-semibold lg:p-1 xl:w-6/12 xl:justify-start">
-                  <span className="">John Doe</span>
-
-                  <Dot
-                    style={{
-                      width: "35px",
-                      height: "35px",
-                    }}
-                  />
-
-                  <span className="text-base">Matemática</span>
-                </div>
-
-                <div className="xl:flex xl:justify-end xl:w-5/12">
-                  <div className="flex items-center justify-center">
-                    <Button
-                      primary
-                      fullWidth
-                      label="VER PROPOSTA"
-                      onClick={() => {}}
-                    />
-                  </div>
-                </div>
-              </div>
+            <div className="flex justify-end w-4/12 px-6 overflow-hidden bg-white group-hover:bg-green-primary  transition ease-in-out delay-150">
+              <span className="rounded-3xl w-12 h-12 bg-green-primary text-white flex justify-center items-center font-semibold group-hover:text-green-primary group-hover:bg-white  transition ease-in-out delay-150">
+                1
+              </span>
             </div>
           </div>
         </div>
 
-        <div className="w-full rounded-lg bg-white p-9 mb-9 shadow-md shadow-[rgba(0,0,0,0.25)]">
-          <h2 className=" text-2xl text-green-primary font-semibold mb-5 md:text-3xl lg:whitespace-nowrap whitespace-normal">
-            Aulas em Andamento
-          </h2>
+        <div className="w-full bg-white hover:bg-green-primary transition ease-in-out delay-150 py-6 group">
+          <div className="flex flex-row w-full">
+            <div className="flex justify-start px-6 w-3/12">
+              <Image
+                src="/assets/images/profile-test.png"
+                alt="Perfil"
+                width={50}
+                height={50}
+                className="object-cover rounded-3xl lg:w-12 lg:h-12"
+              />
+            </div>
 
-          <div className="w-full h-56 scrollbar scrollbar-thumb-slate-100">
-            <div className="w-full rounded-lg bg-green-primary p-5">
-              <div className="flex flex-col lg:flex-row lg:gap-6 xl:w-full">
-                <div className="flex justify-center xl:w-1/12">
-                  <Image
-                    src="/assets/images/profile-test.png"
-                    alt="Perfil"
-                    width={50}
-                    height={40}
-                    className="object-cover rounded-3xl lg:w-12 lg:h-12"
-                  />
-                </div>
+            <div className="flex flex-col w-5/12">
+              <span className="font-semibold">Mary Doe</span>
+              <span className="text-sm whitespace-nowrap">
+                Gostei das aulas, muito...
+              </span>
+            </div>
 
-                <div className="flex items-center justify-center p-2.5 text-white text-lg font-semibold lg:p-1 xl:w-6/12 xl:justify-start">
-                  <span>John Doe</span>
+            <div className="flex justify-end w-4/12 px-6 overflow-hidden bg-white group-hover:bg-green-primary  transition ease-in-out delay-150">
+              <span className="rounded-3xl w-12 h-12 bg-green-primary text-white flex justify-center items-center font-semibold group-hover:text-green-primary group-hover:bg-white  transition ease-in-out delay-150">
+                3
+              </span>
+            </div>
+          </div>
+        </div>
 
-                  <Dot style={{ width: "35px", height: "35px" }} />
+        <div className="w-full bg-white hover:bg-green-primary transition ease-in-out delay-150 py-6 group">
+          <div className="flex flex-row w-full">
+            <div className="flex justify-start px-6 w-3/12">
+              <Image
+                src="/assets/images/profile-test.png"
+                alt="Perfil"
+                width={50}
+                height={50}
+                className="object-cover rounded-3xl lg:w-12 lg:h-12"
+              />
+            </div>
 
-                  <span className="text-base">Matemática</span>
-                </div>
-                <div className="xl:flex xl:justify-end xl:w-5/12">
-                  <div className="flex items-center justify-center">
-                    <Button
-                      primaryMobile
-                      fullWidth
-                      label="VER MENSAGENS"
-                      onClick={() => {}}
-                    />
-                  </div>
-                </div>
-              </div>
+            <div className="flex flex-col w-5/12">
+              <span className="font-semibold">Mary Doe</span>
+              <span className="text-sm whitespace-nowrap">
+                Gostei das aulas, muito...
+              </span>
+            </div>
+
+            <div className="flex justify-end w-4/12 px-6 overflow-hidden bg-white group-hover:bg-green-primary  transition ease-in-out delay-150">
+              <span className="rounded-3xl w-12 h-12 bg-green-primary text-white flex justify-center items-center font-semibold group-hover:text-green-primary group-hover:bg-white  transition ease-in-out delay-150">
+                5
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full bg-white hover:bg-green-primary transition ease-in-out delay-150 py-6 group">
+          <div className="flex flex-row w-full">
+            <div className="flex justify-start px-6 w-3/12">
+              <Image
+                src="/assets/images/profile-test.png"
+                alt="Perfil"
+                width={50}
+                height={50}
+                className="object-cover rounded-3xl lg:w-12 lg:h-12"
+              />
+            </div>
+
+            <div className="flex flex-col w-5/12">
+              <span className="font-semibold">Mary Doe</span>
+              <span className="text-sm whitespace-nowrap">
+                Gostei das aulas, muito...
+              </span>
+            </div>
+
+            <div className="flex justify-end w-4/12 px-6 overflow-hidden bg-white group-hover:bg-green-primary  transition ease-in-out delay-150">
+              <span className="rounded-3xl w-12 h-12 bg-green-primary text-white flex justify-center items-center font-semibold group-hover:text-green-primary group-hover:bg-white  transition ease-in-out delay-150">
+                2
+              </span>
             </div>
           </div>
         </div>
