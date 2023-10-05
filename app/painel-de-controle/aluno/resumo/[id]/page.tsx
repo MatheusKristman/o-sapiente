3385b98d162f18c46d6dd54e0db24a5eac9eb264
João Falcão <jgabfalcao@gmail.com>
2023-10-05 12:19:59 -0300

    fix: Some responsive buttons and user image

--- app/painel-de-controle/aluno/resumo/[id]/page.tsx
+++ app/painel-de-controle/aluno/resumo/[id]/page.tsx
@@ -8,7 +8,7 @@ import Button from "@/components/Button";
 const DashboardPage = () => {
   return (
     <div className="flex-1 w-full px-6 pt-9 mx-auto flex flex-col gap-9 md:flex-row md:px-16 lg:container">
-      <div className="w-full flex flex-col-reverse gap-9 md:flex-col xl:w-6/12">
+      <div className="w-full flex flex-col-reverse gap-9 md:flex-col lg:w-4/12 xl:w-6/12">
         <div className="w-full flex flex-col gap-5 bg-white rounded-2xl p-9 shadow-md shadow-[rgba(0,0,0,0.25)]">
           <div className="relative aspect-square overflow-hidden rounded-2xl">
             <Image
@@ -42,31 +42,39 @@ const DashboardPage = () => {
 
           <div className="w-full h-56 scrollbar scrollbar-thumb-slate-100">
             <div className="w-full rounded-lg bg-white p-5">
-              <div className="flex flex-col lg:flex-row lg:gap-6">
-                <div className="flex justify-center">
+              <div className="flex flex-col lg:flex-row lg:gap-6 xl:w-full">
+                <div className="flex justify-center xl:w-1/12">
                   <Image
                     src="/assets/images/profile-test.png"
                     alt="Perfil"
                     width={50}
                     height={40}
-                    className="object-cover rounded-3xl"
+                    className="object-cover rounded-3xl lg:w-12 lg:h-12"
                   />
                 </div>
 
-                <div className="flex items-center justify-center p-2.5 text-green-primary text-lg font-semibold lg:p-1">
-                  <span>John Doe</span>
+                <div className="flex items-center justify-center p-2.5 text-green-primary text-lg font-semibold lg:p-1 xl:w-6/12 xl:justify-start">
+                  <span className="">John Doe</span>
 
-                  <Dot style={{ width: "35px", height: "35px" }} />
+                  <Dot
+                    style={{
+                      width: "35px",
+                      height: "35px",
+                    }}
+                  />
 
                   <span className="text-base">Matemática</span>
                 </div>
-                <div className="flex items-center justify-center">
-                  <Button
-                    primary
-                    fullWidth
-                    label="VER PROPOSTA"
-                    onClick={() => {}}
-                  />
+
+                <div className="xl:flex xl:justify-end xl:w-5/12">
+                  <div className="flex items-center justify-center">
+                    <Button
+                      primary
+                      fullWidth
+                      label="VER PROPOSTA"
+                      onClick={() => {}}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
@@ -80,118 +88,38 @@ const DashboardPage = () => {
 
           <div className="w-full h-56 scrollbar scrollbar-thumb-slate-100">
             <div className="w-full rounded-lg bg-green-primary p-5">
-              <div className="flex flex-col lg:flex-row lg:gap-6">
-                <div className="flex justify-center">
+              <div className="flex flex-col lg:flex-row lg:gap-6 xl:w-full">
+                <div className="flex justify-center xl:w-1/12">
                   <Image
                     src="/assets/images/profile-test.png"
                     alt="Perfil"
                     width={50}
                     height={40}
-                    className="object-cover rounded-3xl"
+                    className="object-cover rounded-3xl lg:w-12 lg:h-12"
                   />
                 </div>
 
-                <div className="flex items-center justify-center p-2.5 text-white text-lg font-semibold lg:p-1">
+                <div className="flex items-center justify-center p-2.5 text-white text-lg font-semibold lg:p-1 xl:w-6/12 xl:justify-start">
                   <span>John Doe</span>
 
                   <Dot style={{ width: "35px", height: "35px" }} />
 
                   <span className="text-base">Matemática</span>
                 </div>
-                <div className="flex items-center justify-center">
-                  <Button
-                    primaryMobile
-                    fullWidth
-                    label="VER MENSAGENS"
-                    onClick={() => {}}
-                  />
+                <div className="xl:flex xl:justify-end xl:w-5/12">
+                  <div className="flex items-center justify-center">
+                    <Button
+                      primaryMobile
+                      fullWidth
+                      label="VER MENSAGENS"
+                      onClick={() => {}}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
-        {/* TODO continuar estilização */}
-        {/* 
-            <div>
-              <div>
-                <div>
-                  <Image
-                    src="/assets/images/profile-test.png"
-                    alt="Perfil"
-                    width={20}
-                    height={20}
-                    className="object-cover"
-                    />
-                </div>
-
-                <div>
-                  <span>John Doe</span>
-
-                  <Dot />
-
-                  <span>Matemática</span>
-                </div>
-              </div>
-
-              <Button label="VER MENSAGEM" onClick={() => {}} />
-            </div>
-
-            <div>
-              <div>
-                <div>
-                  <Image
-                    src="/assets/images/profile-test.png"
-                    alt="Perfil"
-                    width={20}
-                    height={20}
-                    className="object-cover"
-                  />
-                </div>
-
-                <div>
-                  <span>John Doe</span>
-
-                  <Dot />
-
-                  <span>Matemática</span>
-                </div>
-              </div>
-
-              <Button label="VER MENSAGEM" onClick={() => {}} />
-            </div>
-          </div>
-        
-
-        <div>
-          <h2>Aulas em Andamento</h2>
-
-          <div>
-            <div>
-              <div>
-                <div>
-                  <Image
-                    src="/assets/images/profile-test.png"
-                    alt="Perfil"
-                    width={20}
-                    height={20}
-                    className="object-cover"
-                  />
-                </div>
-
-                <div>
-                  <span>John Doe</span>
-
-                  <Dot />
-
-                  <span>Matemática</span>
-                </div>
-              </div>
-
-              <Button label="VER MENSAGEM" onClick={() => {}} />
-            </div>
-          </div>
-            */}
       </div>
     </div>
   );
