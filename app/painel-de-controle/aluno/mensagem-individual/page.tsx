d611761c2a2baf2adba22a7047947896cfbe25d8
João Falcão <jgabfalcao@gmail.com>
2023-10-17 17:10:44 -0300

    feat: Mobile Modal

--- app/painel-de-controle/aluno/mensagem-individual/page.tsx
+++ app/painel-de-controle/aluno/mensagem-individual/page.tsx
@@ -8,6 +8,7 @@ import {
   MoreHorizontal,
   Plus,
   Video,
+  XCircleIcon,
 } from "lucide-react";
 
 import Button from "@/components/Button";
@@ -57,95 +58,30 @@ const DashboardPage = () => {
               <span className="text-md font-medium"> John Doe</span>
               <span className="text-xs">Online</span>
             </div>
-            <button
-              onClick={openModal}
-              className="text-green-primary px-4 flex items-center ml-auto md:hidden"
-            >
-              <MoreHorizontal size={35} />
+
+            <button className="text-green-primary px-4 flex items-center ml-auto md:hidden">
+              {isModalOpen ? (
+                <XCircleIcon onClick={closeModal} size={35} />
+              ) : (
+                <MoreHorizontal onClick={openModal} size={35} />
+              )}
             </button>
-            {isModalOpen && (
-              <div
-                tabIndex={-1}
-                className="fixed top-0 left-0 right-0 z-50 md:hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
-              >
-                <div className="relative w-full max-w-2xl max-h-full">
-                  {/* <!-- Modal content --> */}
-                  <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    {/* <!-- Modal header --> */}
-                    <div className="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
-                      <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-                        Top right modal
-                      </h3>
-                      <button
-                        onClick={closeModal}
-                        type="button"
-                        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-hide="top-right-modal"
-                      >
-                        <svg
-                          className="w-3 h-3"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 14 14"
-                        >
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                          />
-                        </svg>
-                        <span className="sr-only">Close modal</span>
-                      </button>
-                    </div>
-                    {/* <!-- Modal body --> */}
-                    <div className="p-6 space-y-6">
-                      <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                        With less than a month to go before the European Union
-                        enacts new consumer privacy laws for its citizens,
-                        companies around the world are updating their terms of
-                        service agreements to comply.
-                      </p>
-                      <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                        The European Union’s General Data Protection Regulation
-                        (G.D.P.R.) goes into effect on May 25 and is meant to
-                        ensure a common set of data rights in the European
-                        Union. It requires organizations to notify users as soon
-                        as possible of high-risk data breaches that could
-                        personally affect them.
-                      </p>
-                    </div>
-                    {/* <!-- Modal footer --> */}
-                    <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                      <button
-                        onClick={closeModal}
-                        data-modal-hide="top-right-modal"
-                        type="button"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                      >
-                        I accept
-                      </button>
-                      <button
-                        onClick={closeModal}
-                        data-modal-hide="top-right-modal"
-                        type="button"
-                        className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-                      >
-                        Decline
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )}
 
             <div className="hidden md:flex justify-end ml-auto px-4 py-2">
               <Button label="Confirmar Finalização" fullWidth primary />
             </div>
           </div>
         </div>
+
+        {isModalOpen && (
+          <div className="flex w-full">
+            <div className="flex w-full justify-end  mt-1 mr-6">
+              <div className="flex justify-center items-center w-72 h-24 bg-white rounded-l-lg rounded-br-lg">
+                <Button label="Confirmar Finalização" primary />
+              </div>
+            </div>
+          </div>
+        )}
 
         <div className="w-full flex bg-[#2C383F] h-28 mt-auto">
           <div className="w-full flex flex-row px-4 py-4 gap-8">
