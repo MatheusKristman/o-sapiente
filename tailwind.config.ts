71f15d675417e9c0b45a692c2e2a62beda6bc3a2
João Falcão <jgabfalcao@gmail.com>
2023-10-23 11:02:30 -0300

    feat: inserting custom table margin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,6 +54,16 @@ const config: Config = {
         "max-height": "max-height",
       },
     },
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      '30': '30px',
+    }
   },
   variants: {
     extend: {
