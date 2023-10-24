52be0d24fb0488ce151fb7b4bdc0441280ad2e1f
João Falcão <jgabfalcao@gmail.com>
2023-10-24 17:36:21 -0300

    fix: select button

--- app/painel-de-controle/aluno/historico-de-solicitacoes/[id]/page.tsx
+++ app/painel-de-controle/aluno/historico-de-solicitacoes/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { Dot, Search } from "lucide-react";
+import { Dot, Search, ChevronDown } from "lucide-react";
 
 import Button from "@/components/Button";
 
@@ -36,45 +36,64 @@ const DashboardPage = () => {
   ];
   return (
     <>
-      <div className="flex flex-col mt-8 gap-6 items-center">
-        <div className="flex flex-col items-center">
-          <h2 className="font-normal text-[#879298] text-xl">Finalizadas</h2>
-          <p className="font-medium text-4xl">10</p>
+      <div className=" w-full px-6 pt-9 mx-auto md:px-16 lg:container">
+        <div className="flex flex-col mt-8 gap-6 items-center md:flex-row md:gap-24 md:mt-12 md:px-14 desktop:px-[120px]">
+          <div className="flex flex-col items-center md:items-start">
+            <h2 className="font-normal text-[#879298] text-xl">Finalizadas</h2>
+            <p className="font-medium text-4xl">10</p>
+          </div>
+
+          <div className="flex flex-col items-center md:items-start">
+            <h2 className="font-normal text-[#879298] text-xl">Em Andamento</h2>
+            <p className="font-medium text-4xl">10</p>
+          </div>
+
+          <div className="flex flex-col items-center md:items-start">
+            <h2 className="font-normal text-[#879298] text-xl">Total</h2>
+            <p className="font-medium text-4xl">10</p>
+          </div>
         </div>
 
-        <div className="flex flex-col items-center">
-          <h2 className="font-normal text-[#879298] text-xl">Em Andamento</h2>
-          <p className="font-medium text-4xl">10</p>
-        </div>
-
-        <div className="flex flex-col items-center">
-          <h2 className="font-normal text-[#879298] text-xl">Total</h2>
-          <p className="font-medium text-4xl">10</p>
-        </div>
-      </div>
-
-      <div className="w-full px-6">
-        <div className="pt-8 relative mx-auto">
-          <input
-            type="search"
-            name="search"
-            placeholder="Faça sua pesquisa aqui..."
-            className="w-full border-2 border-[#C8D6DF] bg-[#C8D6DF] h-10 px-5 pr-16 rounded-lg text-md focus:outline-none"
-          />
-          <button type="submit" className="absolute right-0 top-6 mt-4 mr-4">
-            <Search
-              style={{
-                color: "#9DA5AA",
-              }}
+        <div className="w-full px-6 md:px-14 md:w-7/12 desktop:px-[120px]">
+          <div className="pt-8 relative mx-auto">
+            <input
+              type="search"
+              name="search"
+              placeholder="Faça sua pesquisa aqui..."
+              className="w-full border-2 border-[#C8D6DF] bg-[#C8D6DF] h-10 px-5 pr-16 rounded-lg text-md focus:outline-none"
             />
-          </button>
+            <button type="submit" className="absolute right-0 top-6 mt-4 mr-4">
+              <Search
+                style={{
+                  color: "#9DA5AA",
+                }}
+              />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="flex mt-5 px-6">
-        <Button label="Consultar Certificado" fullWidth primary />
-      </div>
+        <div className="flex flex-col md:flex-row w-full mt-5 px-6 md:pl-14 gap-6">
+          <div className="flex w-full md:w-5/12 md:whitespace-nowrap">
+            <Button label="Consultar Certificado" fullWidth primary />
+          </div>
 
-      <ResponsiveTable data={tableData} />
+          <div className="relative w-full md:w-3/12">
+            <select
+              id="periodo"
+              className="bg-[#2C383F] border border-[#2C383F] text-white text-lg rounded-lg w-full px-5 h-[46px] appearance-none text-center md:text-start"
+            >
+              <option value="30">30 Dias</option>
+              <option value="60">60 Dias</option>
+              <option value="90">90 Dias</option>
+              <option value="120">120 Dias</option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center  px-2 text-white">
+              <ChevronDown size={25} />
+            </div>
+          </div>
+        </div>
+
+        <ResponsiveTable data={tableData} />
+      </div>
     </>
   );
 };
