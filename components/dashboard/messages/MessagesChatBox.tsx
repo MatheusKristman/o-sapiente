8366064206ab01bc97131a9596c2db4c8c812879
João Falcão <jgabfalcao@gmail.com>
2023-11-13 14:04:47 -0300

    fix: Nav modal and Footer modal in messages

--- components/dashboard/messages/MessagesChatBox.tsx
+++ components/dashboard/messages/MessagesChatBox.tsx
@@ -1,15 +1,51 @@
-import { ChevronLeft, MoreHorizontal, Plus, ImageIcon, Video, Mic } from "lucide-react";
+import {
+  ChevronLeft,
+  MoreHorizontal,
+  Plus,
+  ImageIcon,
+  Video,
+  Mic,
+  XCircleIcon,
+} from "lucide-react";
 import Image from "next/image";
 
 import Button from "@/components/Button";
 import { cn } from "@/libs/utils";
+import { useEffect, useState } from "react";
 
 interface MessagesChatBoxProps {
   isMessageOpen: boolean;
   handleBackBtn: () => void;
 }
 
-const MessagesChatBox = ({ isMessageOpen, handleBackBtn }: MessagesChatBoxProps) => {
+const MessagesChatBox = ({
+  isMessageOpen,
+  handleBackBtn,
+}: MessagesChatBoxProps) => {
+  const [isModalNavOpen, setIsModalNavOpen] = useState(false);
+  const [isModalFooterOpen, setIsModalFooterOpen] = useState(false);
+
+  const toggleModalNav = () => {
+    setIsModalNavOpen(!isModalNavOpen);
+  };
+
+  const toggleModalFooter = () => {
+    setIsModalFooterOpen(!isModalFooterOpen);
+  };
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth >= 768) {
+        setIsModalNavOpen(false);
+        setIsModalFooterOpen(false);
+      }
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div
       className={cn(
@@ -20,7 +56,10 @@ const MessagesChatBox = ({ isMessageOpen, handleBackBtn }: MessagesChatBoxProps)
       <div className=" w-full bg-[#2C383F] h-fit px-6 py-4 sm:px-16">
         <div className="w-full flex items-center justify-between">
           <div className="flex items-center gap-x-4">
-            <button onClick={handleBackBtn} className="text-green-primary block lg:hidden">
+            <button
+              onClick={handleBackBtn}
+              className="text-green-primary block lg:hidden"
+            >
               <ChevronLeft size={35} />
             </button>
 
@@ -40,28 +79,94 @@ const MessagesChatBox = ({ isMessageOpen, handleBackBtn }: MessagesChatBoxProps)
           </div>
 
           <div className="flex items-center">
-            <button className="text-green-primary flex items-center ml-auto md:hidden">
-              <MoreHorizontal size={35} strokeWidth={2.7} />
+            <button
+              onClick={toggleModalNav}
+              className="text-green-primary flex items-center ml-auto md:hidden"
+            >
+              {isModalNavOpen ? (
+                <XCircleIcon
+                  onClick={toggleModalNav}
+                  size={35}
+                  strokeWidth={2.7}
+                />
+              ) : (
+                <MoreHorizontal
+                  onClick={toggleModalNav}
+                  size={35}
+                  strokeWidth={2.7}
+                />
+              )}
             </button>
 
             <div className="hidden md:flex justify-end ml-auto">
-              <Button onClick={() => {}} label="Confirmar Finalização" fullWidth primary />
+              <Button
+                onClick={() => {}}
+                label="Confirmar Finalização"
+                fullWidth
+                primary
+              />
             </div>
           </div>
         </div>
       </div>
 
-      <div className="w-full flex bg-[#2C383F] h-28 mt-auto">
+      {isModalNavOpen && (
+        <div className="flex w-full">
+          <div className="flex w-full justify-end  mt-1">
+            <div className="flex w-full justify-end ">
+              <div className="flex justify-center items-center w-72 h-24 bg-white rounded-l-lg rounded-br-lg">
+                <Button
+                  onClick={() => {}}
+                  label="Confirmar Finalização"
+                  primary
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {isModalFooterOpen && (
+        <div className="flex flex-col-reverse justify-start w-[233px] h-full">
+          <div className="flex flex-col gap-4 items-center h-fit bg-white rounded-r-lg rounded-tl-lg p-6">
+            <button
+              onClick={() => {}}
+              className=" rounded-xl gap-2.5 p-2.5 w-44 h-12 bg-green-primary text-white flex justify-start items-center"
+            >
+              <ImageIcon />
+              Enviar Imagem
+            </button>
+            <button
+              onClick={() => {}}
+              className="rounded-xl gap-2.5 p-2.5 w-44 h-12 bg-green-primary text-white flex justify-start items-center"
+            >
+              <Video />
+              Enviar Video
+            </button>
+          </div>
+        </div>
+      )}
+
+      <div className="w-full flex bg-[#2C383F] h-[112px]  mt-auto">
         <div className="w-full flex flex-row px-6 py-4 gap-8 sm:px-16">
           <div className="flex flex-row items-center justify-start gap-3.5">
-            <button className="flex rounded-xl w-12 h-12 bg-green-primary text-white md:hidden justify-center items-center">
-              <Plus />
+            <button
+              onClick={toggleModalFooter}
+              className="flex rounded-xl w-12 h-12 bg-green-primary text-white md:hidden justify-center items-center"
+            >
+              {isModalFooterOpen ? <XCircleIcon /> : <Plus />}
             </button>
-            <button className="hidden rounded-xl w-12 h-12 bg-green-primary text-white md:flex justify-center items-center">
+            <button
+              onClick={() => {}}
+              className="hidden rounded-xl w-12 h-12 bg-green-primary text-white md:flex justify-center items-center"
+            >
               <ImageIcon />
             </button>
 
-            <button className="hidden rounded-xl w-12 h-12 bg-green-primary text-white md:flex justify-center items-center">
+            <button
+              onClick={() => {}}
+              className="hidden rounded-xl w-12 h-12 bg-green-primary text-white md:flex justify-center items-center"
+            >
               <Video />
             </button>
           </div>
