fb4cb4a117bdb4625653a9fa0ad7c72d94338229
MatheusKristman <kristman058@gmail.com>
2024-03-21 11:09:09 -0300

    fixed student message page

---
+++ app/painel-de-controle/aluno/[id]/mensagens/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState } from "react";
+import { useParams } from "next/navigation";
+
+import MessagesEmpty from "@/components/dashboard/messages/MessagesEmpty";
+import MessagesContactsBox from "@/components/dashboard/messages/MessagesContactsBox";
+
+// para componentes das mensagens que são apresentadas na tela, usar o capitulo Chat Messages component do video fullstack discord clone do code with antonio, é uma boa referencia
+
+const MessagesPage = () => {
+  const params = useParams();
+
+  return (
+    <div className="w-full h-[calc(100vh-131px)] min-h-[500px] mx-auto flex flex-col lg:h-[calc(100vh-147px)] lg:flex-row ">
+      <MessagesContactsBox />
+
+      <MessagesEmpty />
+    </div>
+  );
+};
+
+export default MessagesPage;
