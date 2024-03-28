d7ce1eb7972f16f54d32eccfd6764c76ec7958c0
MatheusKristman <kristman058@gmail.com>
2024-03-28 15:47:54 -0300

    added filter function to contact component and added skeletons on components

--- components/dashboard/messages/MessagesContacts.tsx
+++ components/dashboard/messages/MessagesContacts.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Search } from "lucide-react";
-import { useEffect, useMemo, useState } from "react";
+import { ChangeEvent, useEffect, useMemo, useState } from "react";
 import { find } from "lodash";
 
 import { cn } from "@/libs/utils";
@@ -27,7 +27,11 @@ const MessagesContacts = ({
 }: Props) => {
     const [conversations, setConversations] =
         useState<FullConversationType[]>(initialConversations);
+    const [filteredConversations, setFilteredConversations] = useState<
+        FullConversationType[]
+    >([]);
     const [unreadMessages, setUnreadMessages] = useState<number>(0);
+    const [searchValue, setSearchValue] = useState<string>("");
 
     const { isOpen, conversationId } = useConversation(conversationParams);
     const { userId } = useHeaderStore();
@@ -81,6 +85,60 @@ const MessagesContacts = ({
         pusherClient.bind("conversation:update", updateHandler);
     }, [pusherKey, router, userId]);
 
+    useEffect(() => {
+        if (searchValue.length > 2) {
+            setFilteredConversations(
+                conversations.filter(
+                    (conversation) =>
+                        conversation.users[0].firstName
+                            .toLowerCase()
+                            .normalize("NFD")
+                            .replace(/[\u0300-\u036f]/g, "")
+                            .includes(
+                                searchValue
+                                    .normalize("NFD")
+                                    .replace(/[\u0300-\u036f]/g, "")
+                                    .toLowerCase(),
+                            ) ||
+                        conversation.users[0].lastName
+                            .toLowerCase()
+                            .normalize("NFD")
+                            .replace(/[\u0300-\u036f]/g, "")
+                            .includes(
+                                searchValue
+                                    .normalize("NFD")
+                                    .replace(/[\u0300-\u036f]/g, "")
+                                    .toLowerCase(),
+                            ) ||
+                        conversation.users[1].firstName
+                            .toLowerCase()
+                            .normalize("NFD")
+                            .replace(/[\u0300-\u036f]/g, "")
+                            .includes(
+                                searchValue
+                                    .normalize("NFD")
+                                    .replace(/[\u0300-\u036f]/g, "")
+                                    .toLowerCase(),
+                            ) ||
+                        conversation.users[1].lastName
+                            .toLowerCase()
+                            .normalize("NFD")
+                            .replace(/[\u0300-\u036f]/g, "")
+                            .includes(
+                                searchValue
+                                    .normalize("NFD")
+                                    .replace(/[\u0300-\u036f]/g, "")
+                                    .toLowerCase(),
+                            ),
+                ),
+            );
+        }
+    }, [searchValue, conversations]);
+
+    function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+        setSearchValue(event.target.value);
+    }
+
     return (
         <div
             className={cn(
@@ -91,6 +149,8 @@ const MessagesContacts = ({
             <div className="px-9 w-full">
                 <div className="w-full flex items-center justify-between h-[46px] bg-[#EBEFF1] rounded-lg px-5 peer">
                     <input
+                        value={searchValue}
+                        onChange={handleSearch}
                         type="text"
                         name="search"
                         placeholder="Faça sua pesquisa aqui..."
@@ -107,7 +167,23 @@ const MessagesContacts = ({
             </div>
 
             <div className="w-full h-56 scrollbar scrollbar-thumb-slate-100 mt-9">
-                {conversations.length > 0 ? (
+                {searchValue.length > 2 ? (
+                    filteredConversations.length > 0 ? (
+                        filteredConversations.map((conversation) => (
+                            <MessagesContactsBox
+                                key={conversation.id}
+                                conversation={conversation}
+                                selected={conversationId === conversation.id}
+                                userType={userType}
+                                unreadMessages={unreadMessages}
+                            />
+                        ))
+                    ) : (
+                        <span className="mx-auto block text-lg font-medium text-center text-gray-primary/50">
+                            Nenhum contato encontrado
+                        </span>
+                    )
+                ) : conversations.length > 0 ? (
                     conversations.map((conversation) => (
                         <MessagesContactsBox
                             key={conversation.id}
