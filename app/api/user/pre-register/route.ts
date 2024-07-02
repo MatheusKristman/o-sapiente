26760aa6facc3972e834f27b144c0f5a3e1fe382
MatheusKristman <kristman058@gmail.com>
2024-07-02 16:30:58 -0300

    adding email test for register

--- app/api/user/pre-register/route.ts
+++ app/api/user/pre-register/route.ts
@@ -14,11 +14,29 @@ export async function POST(req: Request) {
     const emailPass: string = process.env.EMAIL_PASS!;
     const emailPort: number = Number(process.env.EMAIL_PORT!);
     const body = await req.json();
-    const { firstName, lastName, email, tel, password, passwordConfirm, accountType } = body;
+    const {
+      firstName,
+      lastName,
+      email,
+      tel,
+      password,
+      passwordConfirm,
+      accountType,
+    } = body;
     const baseUrl =
-      process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_BASEURL_DEV : process.env.NEXT_PUBLIC_BASEURL;
+      process.env.NODE_ENV === "development"
+        ? process.env.NEXT_PUBLIC_BASEURL_DEV
+        : process.env.NEXT_PUBLIC_BASEURL;
 
-    if (!firstName || !lastName || !email || !tel || !password || !passwordConfirm || !accountType) {
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !tel ||
+      !password ||
+      !passwordConfirm ||
+      !accountType
+    ) {
       return new NextResponse("Dados inválidos, verifique e tente novamente", {
         status: 401,
       });
@@ -37,7 +55,10 @@ export async function POST(req: Request) {
     }
 
     if (password !== passwordConfirm) {
-      return new NextResponse("Senhas não coincidem, verifique e tente novamente", { status: 401 });
+      return new NextResponse(
+        "Senhas não coincidem, verifique e tente novamente",
+        { status: 401 },
+      );
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
@@ -96,7 +117,7 @@ export async function POST(req: Request) {
         EmailConfirmAccount({
           userName: `${user.firstName} ${user.lastName}`,
           url: `${baseUrl}/?id=${user.id}&confirmed=true&type=student`,
-        })
+        }),
       );
 
       const options = {
@@ -110,11 +131,18 @@ export async function POST(req: Request) {
         if (error) {
           console.log("[ERROR_ON_CONFIRMATION_ACCOUNT_EMAIL]", error);
 
-          return new NextResponse("Ocorreu um erro no envio do e-mail de confirmação da sua conta", {
-            status: 400,
-          });
+          return new NextResponse(
+            "Ocorreu um erro no envio do e-mail de confirmação da sua conta",
+            {
+              status: 400,
+            },
+          );
         }
       });
+
+      console.log(emailUser);
+      console.log(emailPass);
+      console.log(options);
     }
 
     if (accountType === "Professor") {
@@ -133,7 +161,7 @@ export async function POST(req: Request) {
         EmailConfirmAccount({
           userName: `${user.firstName} ${user.lastName}`,
           url: `${baseUrl}/?id=${user.id}&confirmed=true&type=professor`,
-        })
+        }),
       );
 
       const options = {
@@ -147,24 +175,37 @@ export async function POST(req: Request) {
         if (error) {
           console.log("[ERROR_ON_CONFIRMATION_ACCOUNT_EMAIL]", error);
 
-          return new NextResponse("Ocorreu um erro no envio do e-mail de confirmação da sua conta", {
-            status: 400,
-          });
+          return new NextResponse(
+            "Ocorreu um erro no envio do e-mail de confirmação da sua conta",
+            {
+              status: 400,
+            },
+          );
         }
       });
+
+      console.log(emailUser);
+      console.log(emailPass);
+      console.log(options);
     }
 
     if (!user) {
-      return new NextResponse("Ocorreu um erro durante a criação da conta, tente novamente", {
-        status: 400,
-      });
+      return new NextResponse(
+        "Ocorreu um erro durante a criação da conta, tente novamente",
+        {
+          status: 400,
+        },
+      );
     }
 
     return NextResponse.json({ id: user.id });
   } catch (error: any) {
     console.log("[ERROR_PROFESSOR_PRE_REGISTER]", error);
-    return new NextResponse("Ocorreu um erro durante o cadastro, tente novamente!", {
-      status: 400,
-    });
+    return new NextResponse(
+      "Ocorreu um erro durante o cadastro, tente novamente!",
+      {
+        status: 400,
+      },
+    );
   }
 }
