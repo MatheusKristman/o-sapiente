41a2614eaaf3fe06e5253669513c587738e0b922
MatheusKristman <kristman058@gmail.com>
2024-08-14 19:37:18 -0300

    fixed tel format on course webhook

--- app/api/webhook/course/route.ts
+++ app/api/webhook/course/route.ts
@@ -125,11 +125,7 @@ export async function POST(req: Request) {
       const installments = body.data.charges[0].last_transaction.installments;
       const clientEmail = body.data.customer.email;
       const clientName = body.data.customer.name;
-      const clientTel =
-        "+" +
-        body.data.customer.phones.home_phone.country_code +
-        body.data.customer.phones.home_phone.area_code +
-        body.data.customer.phones.home_phone.number;
+      const clientTel = `+${body.data.customer.phones.home_phone.country_code} (${body.data.customer.phones.home_phone.area_code}) ${body.data.customer.phones.home_phone.number}`;
 
       const adminOptions = generateAdminEmailOptions({
         emailUser,
