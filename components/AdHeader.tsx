870d371fb360879ab7ece3166ff994794a2e0979
MatheusKristman <kristman058@gmail.com>
2024-08-16 15:49:56 -0300

    fixed adheader

--- components/AdHeader.tsx
+++ components/AdHeader.tsx
@@ -24,7 +24,7 @@ export default function AdHeader() {
     <section className="w-full bg-green-primary/20 py-4">
       <div className="w-full px-6 flex items-center justify-center sm:px-16">
         <span className="text-sm text-gray-primary text-center">
-          Explore nossos cursos de Direito!{" "}
+          Explore nossos cursos de Direito Constitucional - 2° Fase para OAB!{" "}
           <strong
             onClick={() => scrollTo("cursos")}
             className="font-medium underline cursor-pointer"
