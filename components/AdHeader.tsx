e7decef949d395206797661eaf3237d4f40fb85f
MatheusKristman <kristman058@gmail.com>
2024-08-21 12:50:04 -0300

    changed ad header text size

--- components/AdHeader.tsx
+++ components/AdHeader.tsx
@@ -23,11 +23,19 @@ export default function AdHeader() {
   }
 
   return (
-    <section className={cn("w-full bg-green-primary/20 py-4", pathname !== "/" && "hidden")}>
+    <section
+      className={cn(
+        "w-full bg-green-primary/20 py-4",
+        pathname !== "/" && "hidden",
+      )}
+    >
       <div className="w-full px-6 flex items-center justify-center sm:px-16">
-        <span className="text-sm text-gray-primary text-center">
+        <span className="text-sm text-gray-primary text-center sm:text-base">
           Explore nossos cursos de Direito Constitucional - 2° Fase para OAB!{" "}
-          <strong onClick={() => scrollTo("cursos")} className="font-medium underline cursor-pointer">
+          <strong
+            onClick={() => scrollTo("cursos")}
+            className="font-medium underline cursor-pointer"
+          >
             Inscreva-se
           </strong>{" "}
           agora e aprimore seus conhecimentos com especialistas na área.
