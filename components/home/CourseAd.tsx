6e6acd8711c87a48bc28405d9368be4487f23f26
MatheusKristman <kristman058@gmail.com>
2024-09-24 18:06:28 -0300

    added dynamic function for courseAd

--- components/home/CourseAd.tsx
+++ components/home/CourseAd.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { Course } from "@prisma/client";
